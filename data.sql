7f179e6644c00affe4d883b85cd2fa8e2ecbdd75
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-25 19:08:26 -0400

    added initial db data

---
+++ data.sql
@@ -0,0 +1,11 @@
+-- Tabla: users
+insert into users values (1, 'Leanne Graham', 'Bret', 'Sincere@april.biz');
+insert into users values (2, 'Ervin Howell', 'Antonette', 'Shanna@melissa.tv');
+insert into users values (3, 'Clementine Bauch', 'Samantha', 'Nathan@yesenia.net');
+insert into users values (4, 'Patricia Lebsack', 'Karianne', 'Julianne.OConner@kory.org');
+insert into users values (5, 'Chelsey Dietrich', 'Kamren', 'Lucio_Hettinger@annie.ca');
+insert into users values (6, 'Mrs. Dennis Schulist', 'Leopoldo_Corkery', 'Karley_Dach@jasper.info');
+insert into users values (7, 'Kurtis Weissnat', 'Elwyn.Skiles', 'Telly.Hoeger@billy.biz');
+insert into users values (8, 'Nicholas Runolfsdottir V', 'Maxime_Nienow', 'Sherwood@rosamond.me');
+insert into users values (9, 'Glenna Reichert', 'Delphine', 'Chaim_McDermott@dana.io');
+insert into users values (10, 'Clementina DuBuque', 'Moriah.Stanton', 'Rey.Padberg@karina.biz');
