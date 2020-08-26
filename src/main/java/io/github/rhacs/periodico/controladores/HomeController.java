74d3f95204fb34409bb00507bf103201539dc1ff
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-25 22:04:48 -0400

    fixed data load methods
    fixed mapping paths

--- src/main/java/io/github/rhacs/periodico/controladores/HomeController.java
+++ src/main/java/io/github/rhacs/periodico/controladores/HomeController.java
@@ -1,6 +1,8 @@
 package io.github.rhacs.periodico.controladores;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +20,12 @@ import org.springframework.web.client.RestTemplate;
 import io.github.rhacs.periodico.Constantes;
 import io.github.rhacs.periodico.modelos.Comentario;
 import io.github.rhacs.periodico.modelos.Post;
+import io.github.rhacs.periodico.modelos.Usuario;
+import io.github.rhacs.periodico.modelos.dto.ComentarioDTO;
+import io.github.rhacs.periodico.modelos.dto.PostDTO;
 import io.github.rhacs.periodico.repositorios.ComentariosRepositorio;
 import io.github.rhacs.periodico.repositorios.PostsRepositorio;
+import io.github.rhacs.periodico.repositorios.UsuariosRepositorio;
 
 @Controller
 @RequestMapping(path = "/")
@@ -42,53 +48,117 @@ public class HomeController {
     @Autowired
     private PostsRepositorio postsRepositorio;
 
+    /**
+     * Objeto {@link UsuariosRepositorio} que contiene los métodos de manipulación y
+     * consulta relativos a los registros de {@link Usuario}s
+     */
+    @Autowired
+    private UsuariosRepositorio usuariosRepositorio;
+
     // Métodos
     // -----------------------------------------------------------------------------------------
 
     /**
-     * Efectúa la carga de los datos a los repositorios correspondientes
+     * Efectúa la carga de los Posts al repositorio
      */
-    private void cargarDatos() {
-        // Limpiar los repositorios
-        postsRepositorio.deleteAll();
-        comentariosRepositorio.deleteAll();
-
+    private void cargarPosts() {
         // Inicializar RestTemplate
         RestTemplate rest = new RestTemplate();
 
         // Efectuar la petición a la API de Posts y capturar la respuesta
-        ResponseEntity<Post[]> respuesta = rest.getForEntity(Constantes.API_POSTS, Post[].class);
+        ResponseEntity<PostDTO[]> respuesta = rest.getForEntity(Constantes.API_POSTS, PostDTO[].class);
 
         // Extraer datos
-        List<Post> posts = Arrays.asList(respuesta.getBody());
+        List<PostDTO> posts = Arrays.asList(respuesta.getBody());
+        List<Post> ps = new ArrayList<>();
 
-        // Guardar posts en el repositorio
-        posts.forEach(post -> postsRepositorio.save(post));
+        // Buscar todos los usuarios
+        List<Usuario> usuarios = usuariosRepositorio.findAll();
 
-        // Efectuar petición a la API de Comentarios y capturar respuesta
-        ResponseEntity<Comentario[]> resp = rest.getForEntity(Constantes.API_COMENTARIOS, Comentario[].class);
+        // Convertir los datos y guardar en repositorio
+        posts.forEach(post -> {
+            // Inicializar usuario
+            Usuario usuario = null;
 
-        // Extraer los datos
-        List<Comentario> comentarios = Arrays.asList(resp.getBody());
+            // Buscar usuario
+            for (Usuario u : usuarios) {
+                if (u.getId().equals(post.getUserId())) {
+                    usuario = u;
+                    break;
+                }
+            }
 
-        // Almacenar los registros en el repositorio
-        comentarios.forEach(comentario -> comentariosRepositorio.save(comentario));
+            // Crear nuevo Post
+            Post p = new Post(post.getId(), post.getTitle(), post.getBody(), usuario, new HashSet<>());
+
+            // Agregar al listado
+            ps.add(p);
+        });
+
+        // Guardar por lote
+        postsRepositorio.saveAll(ps);
+    }
+
+    private void cargarComentarios() {
+        // Iniciar RestTemplate
+        RestTemplate rest = new RestTemplate();
+
+        // Efectuar petición a la API y capturar respuesta
+        ResponseEntity<ComentarioDTO[]> respuesta = rest.getForEntity(Constantes.API_COMENTARIOS, ComentarioDTO[].class);
+
+        // Extraer datos
+        List<ComentarioDTO> comentarios = Arrays.asList(respuesta.getBody());
+
+        // Inicializar listado de Comentarios
+        List<Comentario> comms = new ArrayList<>();
+
+        // Buscar todos los posts
+        List<Post> posts = postsRepositorio.findAll();
+
+        // Convertir datos obtenidos
+        comentarios.forEach(comentario -> {
+            // Buscar post
+            for(Post p : posts) {
+                if(p.getId().equals(comentario.getPostId())) {
+                    // Crear nuevo comentario
+                    Comentario c = new Comentario(comentario.getId(), comentario.getName(), comentario.getEmail(), comentario.getBody(), p);
+
+                    // Agregar al listado
+                    comms.add(c);
+
+                    break;
+                }
+            }
+        });
+
+        // Guardar registros por lote
+        comentariosRepositorio.saveAll(comms);
     }
 
     // Solicitudes GET
     // -----------------------------------------------------------------------------------------
 
     /**
-     * Muestra el listado de publicaciones disponibles en el repositorio
+     * Efectura la carga de datos y redirecciona a la página principal
+     * 
+     * @return un objeto {@link String} con el nombre de la vista
+     */
+    @GetMapping
+    public String cargarDatos() {
+        cargarPosts();
+        cargarComentarios();
+
+        return "redirect:/posts";
+    }
+
+    /**
+     * Muestra el listado de publicaciones almacenadas en el repositorio
      * 
      * @param modelo objeto {@link Model} que contiene el modelo de la vista
      * @return un objeto {@link String} que contiene el nombre de la vista
      */
-    @GetMapping
-    public String listarPublicaciones(Model modelo) {
-        // Cargar los datos
-        cargarDatos();
-
+    @GetMapping(path = "/posts")
+    public String verListado(Model modelo) {
         // Buscar listado de publicaciones
         List<Post> posts = postsRepositorio.findAll(Sort.by(Direction.DESC, "id"));
 
@@ -106,7 +176,7 @@ public class HomeController {
      * @param modelo objeto {@link Model} que contiene el modelo de la vista
      * @return un objeto {@link String} que contiene el nombre de la vista
      */
-    @GetMapping(path = "/post/{id:^[0-9]+$}")
+    @GetMapping(path = "/posts/{id:^[0-9]+$}")
     public String verDetalles(@PathVariable Long id, Model modelo) {
         // Buscar información del Post
         Optional<Post> post = postsRepositorio.findById(id);
@@ -114,7 +184,7 @@ public class HomeController {
         // Verificar si existe
         if (post.isPresent()) {
             // Agregar post al modelo
-            modelo.addAttribute("post", post);
+            modelo.addAttribute("post", post.get());
 
             // Devolver vista
             return "detalles";
